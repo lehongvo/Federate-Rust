c9c702623d2da63aa532ab0097b16050f6ee5acd
vincentVo <volh@smartosc.com>
2026-03-02 14:54:46 +0700

    New Init Project

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -2,5 +2,20 @@ fn main() {
     let a: u64 = 10;
 
     println!("a! is equal to {:?}", a);
-    dbg!(a);
+}
+
+/**
+ * This is a public function.
+ *
+ * # Arguments
+ *
+ * * `a` - A u64 value.
+ *
+ * # Returns
+ *
+ * * `a` - A u64 value.
+ */
+pub fn public_function(a: u64) -> u64 {
+    println!("public_function");
+    a
 }
